79529597e16f004b2ce300907f4f7f142ccb2d9c
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-07-21 14:49:40 +0500

    a

--- HT8_1/Program.cs
+++ HT8_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1.LinkedList
+
+//    1 uquv markazdagi umumiy uquvchilar ruyhatini saqlashda yani 	 	
+//	qushish va uchirib yuborish ancha osonn
+//2stack
+//	1 liftga oxirgi kirgan birinchi chiqib ketadi
+//	2 internettan malumotralni izlaganimizda oxirgi malumot birinchi 
+//	chiqib qoladi
+//	3 aftobusga chiqich tushish
+//3queue - navbat
+//	1 onlayn marketda 1 - zakazqilgan bulsa ushani malumotlari 	birinchi yoziladi
+//	2 library dasturida qushilgan kitoplarni birinchi bulib 	qushilganni birinchi ochiladi
+//	3 aftobusda birinchi bulib yulga chiqqan aftobus kartada birinchi 	kurinadi
+//4 dictianary
+//	1.Tulov tizimida key ga pul miqdori va value currency sini saqlash
+//	2.colormania misolida rangi keyga hash codi value
+//	3.clicda keyga mobil operatorlar valuega amallar
+//5list
+//	birnacha hil malumotlarni saqlash user lar misolida
+//	birnecha uquvchilarni saqlash
+//	bir qancha noutbook malumotlarini
---
+++ HT9_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+List<string> Emails = new List<string>();
+Emails.Add("john.doe @example.com");
+Emails.Add("abdurahmonsadriddinov0412@gmail.com");
+Emails.Add("john.doetag@example.com");
+Emails.Add("test.com");
+Emails.Add("sherali23@.com");
+
+
+var emailAddressRegex = new Regex("^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$");
+//var emailAddressPattern = @"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$";
+foreach (string email in Emails)
+{
+    if (emailAddressRegex.IsMatch(email))
+    {
+        Console.WriteLine(email + " => Valid email");
+    }
+    else
+    {
+        Console.WriteLine(email + " => invalid email");
+    }
+}
+
+
+//Console.WriteLine(Regex.IsMatch(emailAddress, emailAddressPattern));
+//Console.WriteLine(emailAddressRegex.IsMatch(emailAddress));
