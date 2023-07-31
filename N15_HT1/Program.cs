1fc4a8a076704248363e82c83b9bc27655c15528
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-07-31 14:22:51 +0500

    31/0723/14:22

--- N15_HT1/Program.cs
+++ N15_HT1/Program.cs
@@ -12,9 +12,9 @@
 
 
 var Day1 = new UltimateWeatherReport();
-Day1.AddWeatherInformation(new DateOnly(2023, 08, 29), "Bugun havo ancha issiq");
-Day1.AddWeatherInformation(new DateOnly(2023, 08, 30), "Ertaga havo harorati uzgarib turadi");
-Day1.AddWeatherInformation(new DateOnly(2023, 08, 25), "Abidgacha dimm bulatti Abittan kiyin jijja yomgir yogib rasa osh yikon havo buladi");
+Day1.AddWeatherInformation(new DateOnly(2023, 08, 9), "Bugun havo ancha issiq");
+Day1.AddWeatherInformation(new DateOnly(2023, 08, 10), "Ertaga havo harorati uzgarib turadi");
+Day1.AddWeatherInformation(new DateOnly(2023, 08, 8), "Abidgacha dimm bulatti Abittan kiyin jijja yomgir yogib rasa osh yikon havo buladi");
 Day1.AddWeatherInformation(new DateOnly(2023, 08, 1), "Endi urtogla erta toza ochu havo buluriddi");
 Day1.AddWeatherInformation(new DateOnly(2023, 08, 2), "Abidgacha nam havo buladi abiddan kiyin kam havo buladi");
 Day1.AddWeatherInformation(new DateOnly(2023, 08, 3), "garbdan shamol yomon esadi oma likin");
@@ -25,16 +25,16 @@ Day1.AddWeatherInformation(new DateOnly(2023, 08, 7), "Qor yogsa kerak");
 
 
 
-var GetWet1 = Day1.GetWeather(new DateOnly(2023,07,29),6);
-foreach(var b in GetWet1)
+var GetWet1 = Day1.GetWeather(new DateOnly(2023, 08, 1), 5);
+foreach (var b in GetWet1)
 {
     Console.WriteLine(b);
 }
 
-Console.WriteLine();
-var Getwether2  = Day1.GetWeather(6);
-foreach (var b in Getwether2)
-{
-    Console.WriteLine(b);
-}
+//Console.WriteLine();
+//var Getwether2  = Day1.GetWeather(9);
+//foreach (var b in Getwether2)
+//{
+//    Console.WriteLine(b);
+//}
 
