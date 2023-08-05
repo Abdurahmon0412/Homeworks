45663d7bb34f83886b360ffc91885c8c067c4d41
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-08-05 14:03:11 +0500

    N191 is done

---
+++ N19_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using N19_1;
+
+//for name
+if (Validator.IsValidName("  Abdurahmon    ", out var formattedName))
+    Console.WriteLine($"fixed name - {formattedName}");
+else
+    Console.WriteLine("Invalid name is not fixed");
+
+
+// for email
+if (Validator.IsValidEmailadress("           test@gamail.com", out var formattedEmailAddress))
+    Console.WriteLine($"true fixedEmailAddress - {formattedEmailAddress}");
+else
+    Console.WriteLine("Invalid email is not fixed");
+
+//for phone number
+if (Validator.IsValidPhoneNumber("      +998938399777   ", out var formattedPhoneNumber))
+    Console.WriteLine($"true fixed Phone Number - {formattedPhoneNumber}");
+else
+    Console.WriteLine("Invalid phone number is not fixed");
+
+
+//for age
+var age = Console.ReadLine();
+if (Validator.IsValidAge(age))
+    Console.WriteLine($"valid age");
+else
+    Console.WriteLine("Invalid invalid is not fixed");
+
+
---
+++ N19_1/Validator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace N19_1
+{
+    public static class Validator
+    {
+        public static bool IsValidName(in string name, out string formattedName)
+        {
+            formattedName = string.Empty;
+            if(string.IsNullOrWhiteSpace(name)) return false;
+
+            formattedName = name.Trim();
+            if (Regex.IsMatch(formattedName, "^[A-Za-z][A-Za-z0-9]*$"))
+                return true;
+            return false;
+        }
+
+
+        public static bool IsValidEmailadress(in string emailAddress, out string formattedEmailAddress)
+        {
+            formattedEmailAddress = string.Empty;
+            if (string.IsNullOrWhiteSpace(emailAddress)) return false;
+
+            formattedEmailAddress = emailAddress.Trim();
+            return Regex.IsMatch(formattedEmailAddress, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+        }
+
+
+        public static bool IsValidAge(in string age)
+        {
+            try
+            {
+
+                if (Convert.ToInt32(age) < 1)
+                {
+                    return false;
+
+                }
+                else
+                {
+                    return true;
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool IsValidPhoneNumber(in string phoheNumber, out string formattedPhoneNumber)
+        {
+            formattedPhoneNumber = string.Empty;
+            if (string.IsNullOrWhiteSpace(phoheNumber)) return false;
+
+            formattedPhoneNumber = phoheNumber.Trim();
+            if (Regex.IsMatch(formattedPhoneNumber, @"^\+998\d{9}$"));
+            return false;
+        }
+    }
+}
